96869e62d20041d673accb719d9962f63cbd8972
sanja <sanja@ebi.ac.uk>
2018-03-02 10:33:13 +0000

    removed version setting

--- cif_emdb_translator.py
+++ cif_emdb_translator.py
@@ -46,7 +46,8 @@ class CifEMDBTranslator(object):
         There are many constants in use for the translation.
         They have been collected here for ease of use.
         """
-        XML_OUT_VERSION = '3.0.0.0'
+        # the XML version is now read from emdb.py
+        # XML_OUT_VERSION = '3.0.0.0'
 
         # Cif categories
         CITATION = 'citation'
@@ -9355,7 +9356,8 @@ class CifEMDBTranslator(object):
                 XSD: <xs:attribute name="version" use="required">
                 NOT IN CIF: The value is added in here - 
                 """
-                self.xml_out.set_version(const.XML_OUT_VERSION)
+                # version is already set; it's a fixed value
+                # self.xml_out.set_version(const.XML_OUT_VERSION)
 
             def set_el_admin():
                 """
