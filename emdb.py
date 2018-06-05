7e476f4228d4502a501474a5fd94fd04f54a6740
sanja <sanja@ebi.ac.uk>
2018-06-05 13:07:30 +0100

    change to allowed_nominal_cs range: 0 - 20, inclusive

--- emdb.py
+++ emdb.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 #
-# Generated Tue Jun  5 12:37:24 2018 by generateDS.py version 2.29.5.
+# Generated Tue Jun  5 13:06:56 2018 by generateDS.py version 2.29.5.
 # Python 2.7.11 (v2.7.11:6d1b6a68f775, Dec  5 2015, 12:54:16)  [GCC 4.2.1 (Apple Inc. build 5666) (dot 3)]
 #
 # Command line options:
